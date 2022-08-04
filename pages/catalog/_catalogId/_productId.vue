aa3a14085d44eba1c2bf8edfe75001cb9c45dc2e
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-08-04 09:44:52 +0300

    fixes

--- pages/catalog/_catalogId/_productId.vue
+++ pages/catalog/_catalogId/_productId.vue
@@ -386,7 +386,7 @@
       product: null,
       prodsSlider: null
     }),
-    async fetch() {
+    async mounted() {
       this.product = await this.$axios.$get(`catalog/detail.php?code=${this.$route.params.productId}`);
       this.addBreadcrumbs([
         {
@@ -406,8 +406,6 @@
         },
       ])
       // console.log(this.product);
-    },
-    mounted() {
       if (window.innerWidth <= 1280) {
         setTimeout(() => {
           this.prodsSlider = new Glide('.productPage__analogs .values.glide', {
