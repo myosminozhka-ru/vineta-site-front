163dbcafc066d6d5089ecb321eb688d3f773a94c
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-08-04 11:14:29 +0300

    fixes

--- plugins/print.js
+++ plugins/print.js
@@ -12,18 +12,7 @@ const options = {
       'scrollbars=yes'
     ],
     styles: [
-      'http://62.109.13.15:3000/_nuxt/css/a388784.css',
-      'http://62.109.13.15:3000/_nuxt/css/0f46fbb.css',
-      'http://62.109.13.15:3000/_nuxt/css/1629377.css',
-      'http://62.109.13.15:3000/_nuxt/css/70f033f.css',
-      'http://62.109.13.15:3000/_nuxt/css/d6fa115.css',
-      'http://62.109.13.15:3000/_nuxt/css/d535a41.css',
-      'http://62.109.13.15:3000/_nuxt/css/4180f79.css',
-      'http://62.109.13.15:3000/_nuxt/css/a638625.css"',
-      'http://62.109.13.15:3000/_nuxt/css/bbabfd0.css',
-      'http://62.109.13.15:3000/_nuxt/css/4c9a249.css',
-      'http://62.109.13.15:3000/_nuxt/css/de09dba.css',
-      'http://62.109.13.15:3000/_nuxt/css/e6a3d2e.css',
+      'http://62.109.13.15:3000/_nuxt/app.css',
     ],
     timeout: 1000, // default timeout before the print window appears
     autoClose: true, // if false, the window will not close after printing
