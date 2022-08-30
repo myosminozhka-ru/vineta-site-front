f3fee46029c9d74743ab4e47bcd6f5a68654c2e4
Егор Астрейко <e.astreiko@itrack.ru>
2022-08-30 09:38:59 +0300

    Изменения в ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    apps: [
-      {
-        name: 'vineta',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'dev',
-        env: {
-          "HOST": "127.0.0.1",
-          "PORT": 3000,
-          "NODE_ENV": "production",
-        }
-      }
-    ]
-  }
+  apps: [
+    {
+      name: 'vineta',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+      env: {
+        HOST: '127.0.0.1',
+        PORT: 3000,
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
