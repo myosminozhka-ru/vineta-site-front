d2b2266aba6f3f7e4d73843fde75221f0dc3069f
baltazors <baltazorneo@gmail.com>
2022-11-03 12:32:21 +0200

    add vineta site files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    jquery: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+  },
+}
