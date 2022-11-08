e7d61da51cd4a10191e5a51d905b153b4abf53c7
arstanbek <arstanbek353@gmail.com>
2022-11-08 12:36:14 +0600

    fix sintax error

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,25 @@
     <div>
         <osm-header />
         <nuxt />
-        <script type="text/javascript">
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js";, "ym");
+        <no-ssr>
+          <script type="text/javascript">
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+              if (document.scripts[j].src === r) { 
+                return;
+              }
+            }
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-          ym(13997074, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true
-          });
-        </script>
+            ym(13997074, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+            });
+          </script>
+        </no-ssr>
     </div>
 </template>
 
